3bd359e0c7dcc5c4cb63bbf56b8d83c7968450d0
Joyce Ribeiro <joyribeirogxavier@gmail.com>
2023-11-27 18:50:15 -0300

    adicionando likes e encapsulando nome e likes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome= nome
+        self.__nome= nome
         self.ano=ano
         self.temporadas=temporadas
+        self.__likes = 0
