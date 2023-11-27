4c00c112e1396cb0024821645ab918b1b7d6cf04
Joyce Ribeiro <joyribeirogxavier@gmail.com>
2023-11-27 18:52:28 -0300

    Revert "adicionando likes e encapsulando nome e likes"
    This reverts commit 3bd359e0c7dcc5c4cb63bbf56b8d83c7968450d0.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.__nome= nome
+        self.nome= nome
         self.ano=ano
         self.temporadas=temporadas
-        self.__likes = 0
