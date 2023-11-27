1b1ceed9bdd08c1c17feb96d3929c583186e036d
Joyce Ribeiro <joyribeirogxavier@gmail.com>
2023-11-27 19:31:05 -0300

    encapsulando classe programa

--- main.py
+++ main.py
@@ -2,4 +2,19 @@ class Programa:
     def __init__(self, nome, ano):
         self.__nome =nome.title()
         self.ano = ano
-        self._likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+    
+    def dar_likes(self):
+        self.__likes +=1
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
