a61428bb75892eeb2d7b135097b52abf38b6592b
Joyce Ribeiro <joyribeirogxavier@gmail.com>
2023-11-27 18:58:26 -0300

    encapsulando nome e likes

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ class Serie:
         self.ano=ano
         self.temporadas=temporadas
         self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
+        self.__likes +=1
+
+    @property 
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome =novo_nome.title()
+
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
