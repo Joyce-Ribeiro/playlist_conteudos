eb159c2b9134c3d6eb316e300cb80602a0c259e9
Joyce Ribeiro <joyribeirogxavier@gmail.com>
2023-11-27 18:40:52 -0300

    encapsulando nome e likes

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes=0
+        self.__likes=0
+
+    @property
+    def likes(self):
+        return self.__likes
+    
+    def dar_likes(self):
+        self.__likes +=1
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(vingadores.nome)
